981858b33ee788594b367f1640f6ff2456354d21
ReiDaSnaipada <87833620+ReiDaSnaipada@users.noreply.github.com>
2022-08-10 14:19:31 -0300

    Add files via upload

---
+++ entity soma1 is.vhd
@@ -0,0 +1,21 @@
+entity soma1 is
+    port (
+            cin   : in bit;
+            a     : in bit;
+            b     : in bit;
+            s     : out bit;
+            cout  : out bit;
+    end soma1; 
+
+    architecture a of soma1 is
+        
+    begin
+        s     <= a xor b xor cin;
+        cout  <= (a and b) or (a and cin) or (b and cin);
+        
+        
+    end architecture a;
+
+ soma  1s   <= a xor b xor cin;
+ cout <= (a and b) or (a and cin) or (b and cin); 
+    )
